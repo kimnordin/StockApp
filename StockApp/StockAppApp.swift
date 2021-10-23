4a9adceab65494fa1e19aaf7cd1d2d5f76787b94
kimnordin <kim.nordin97@gmail.com>
2021-10-23 23:24:59 +0200

    Initial Commit

---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Kim Nordin on 2021-10-23.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
